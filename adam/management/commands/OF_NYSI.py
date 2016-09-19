1a572b62fb2d5cf15d3316ee886cc78b9a9c3f07
jdovi <jesse@dovimotors.com>
2016-09-19 10:48:16 -0400

    reminder card functionality

--- adam/management/commands/OF_NYSI.py
+++ adam/management/commands/OF_NYSI.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 from mysite.settings import ADAM_PATH, ADAM_EXPORT_PATH
 import pandas as pd
 import numpy as np
+import pdb
 
 class Command(BaseCommand):
 
@@ -19,65 +20,64 @@ class Command(BaseCommand):
         vidfile['DELDATE'] = pd.to_datetime(vidfile['DELDATE'])
 
         #RM101
-        RM101_start_date = '2016-01-31'
-        RM101_end_date = '2016-03-01'
+        RM101_start_date = '2016-04-30'
+        RM101_end_date = '2016-06-01'
         RM101 = history[history['DATE']> RM101_start_date]
         RM101 = RM101[RM101['DATE']< RM101_end_date]
         RM101 = RM101[RM101['KIT']=='101']
         RM101 = RM101[RM101['KITSUFF']=='RM']
         RM101['TYPE'] = 'Oil & Filter Change'
-        RM101 = RM101[['VEHICLE','TYPE']]
-
+        RM101 = RM101[['VEHICLE','TYPE']].drop_duplicates()
         
         #WRKS
-        WRKS_start_date = '2016-01-31'
-        WRKS_end_date = '2016-03-01'
+        WRKS_start_date = '2016-04-30'
+        WRKS_end_date = '2016-06-01'
         WRKS = history[history['DATE']> WRKS_start_date]
         WRKS = WRKS[WRKS['DATE']< WRKS_end_date]
         WRKS = WRKS[WRKS['KIT']=='WRKS']
         WRKS = WRKS[WRKS['KITSUFF']=='RM']
         WRKS['TYPE'] = 'The Works'
-        WRKS = WRKS[['VEHICLE','TYPE']]
+        WRKS = WRKS[['VEHICLE','TYPE']].drop_duplicates()
         
 
         #101D
-        RM101D_start_date = '2015-11-30'
-        RM101D_end_date = '2016-01-01'
+        RM101D_start_date = '2016-02-29'
+        RM101D_end_date = '2016-04-01'
         RM101D = history[history['DATE']> RM101D_start_date]
         RM101D = RM101D[RM101D['DATE']< RM101D_end_date]
         RM101D = RM101D[RM101D['KIT']=='101D']
         RM101D = RM101D[RM101D['KITSUFF']=='RM']
         RM101D['TYPE'] = 'Diesel Oil & Filter Change'
-        RM101D = RM101D[['VEHICLE','TYPE']]
+        RM101D = RM101D[['VEHICLE','TYPE']].drop_duplicates()
 
         #RM103
-        RM103_start_date = '2015-05-31'
-        RM103_end_date = '2015-07-01'
+        RM103_start_date = '2015-08-31'
+        RM103_end_date = '2015-10-01'
         RM103 = history[history['DATE']> RM103_start_date]
         RM103 = RM103[RM103['DATE']< RM103_end_date]
         RM103 = RM103[RM103['KIT']=='103']
         RM103 = RM103[RM103['KITSUFF']=='RM']
         RM103['TYPE'] = 'New York State Inspection'
-        RM103 = RM103[['VEHICLE','TYPE']]
+        RM103 = RM103[['VEHICLE','TYPE']].drop_duplicates()
 
         #OF Delivery Date
-        OF_del_start_date = '2016-01-31'
-        OF_del_end_date = '2016-03-01'
+        OF_del_start_date = '2016-04-30'
+        OF_del_end_date = '2016-06-01'
         OFDD = vidfile[vidfile['DELDATE']>OF_del_start_date]
         OFDD = OFDD[OFDD['DELDATE']<OF_del_end_date]
         OFDD['VEHICLE'] = OFDD['VID']
         OFDD['TYPE'] = 'The First Service'
-        OFDD = OFDD[['VEHICLE','TYPE']]
+        OFDD = OFDD[['VEHICLE','TYPE']].drop_duplicates()
 
 
         #NYSI Delivery Date
-        NYSI_del_start_date = '2015-05-31'
-        NYSI_del_end_date = '2015-07-01'
+        NYSI_del_start_date = '2015-08-31'
+        NYSI_del_end_date = '2015-10-01'
         NYSI = vidfile[vidfile['DELDATE']>NYSI_del_start_date]
         NYSI = NYSI[NYSI['DELDATE']<NYSI_del_end_date]
         NYSI['VEHICLE'] = NYSI['VID']
         NYSI['TYPE'] = 'New York State Inspection'
-        NYSI = NYSI[['VEHICLE','TYPE']]
+        NYSI = NYSI[['VEHICLE','TYPE']].drop_duplicates()
 
         #Combine resulting dataframes and get address info
         exclusion_list_lname = ['PURGE','DOVI MOTORS INC']
@@ -98,12 +98,15 @@ class Command(BaseCommand):
         all_data = all_data[all_data['STATE'].isin(inclusion_list_state)]
 
         #sortby ZIP and export
-        cols_to_export = ['YEAR','MAKE','MODEL','CUSTLNAME','CUSTFNAME','ADDRESS1','ADDRESS2','CITY','STATE','ZIP','TYPE']
+        cols_to_export = ['YEAR','MAKE','MODEL','VIN','CUSTLNAME','CUSTFNAME','ADDRESS1','ADDRESS2','CITY','STATE','ZIP','TYPE']
         all_data = all_data[cols_to_export]
-        all_data = all_data.sort(columns='ZIP')
+        all_data.columns = ['YEAR_0','MAKE_0','MODEL_0','VIN_0','CUSTLNAME_0','CUSTFNAME_0','ADDRESS1_0','ADDRESS2_0','CITY_0','STATE_0','ZIP_0','TYPE_0']
+        all_data = all_data.sort(columns='ZIP_0')
         output = 'C:\Users\jesse\Desktop\output.csv'
         #all_data.to_csv(output,index=False)
-        g = all_data.groupby(np.arange(len(all_data)) // 8).apply(lambda x: x.reset_index(0, drop=True))
+        group_size = len(all_data)/4
+
+        g = all_data.groupby(np.arange(len(all_data)) // group_size).apply(lambda x: x.reset_index(0, drop=True))
         suffix = 0
 
         final_df = g.iloc[(g.index.get_level_values(0)==0)]
